78fac0ab774abd4a133b8ae8d7c40d438bc90dcf
z5033544 <z5033544@cse.unsw.edu.au>
2020-01-09 17:00:55 +1100

    Implemented 'get' grayscale and hv histogram features

--- feature/feature_defintions/Histogram.py
+++ feature/feature_defintions/Histogram.py
@@ -1,7 +1,8 @@
+import numpy as np
+
 from feature.feature_defintions.Base import FeatureBase
 from skimage.color import rgb2gray, rgb2hsv
 
-import numpy as np
 
 
 class Histogram(FeatureBase):
@@ -11,7 +12,7 @@ class Histogram(FeatureBase):
 
     # 1 channel grayscale histogram feature
     def get_l_feature(self, image):
-        # Convert image to gray-scale
+        # Convert image to 8-bit gray-scale
         image = rgb2gray(image)
         image = np.uint8(image * 255)
 
@@ -19,14 +20,30 @@ class Histogram(FeatureBase):
         gray_histogram = np.histogram(a=image, bins=range(0, self.bins))
 
         # Normalize histogram
-        gray_histogram =
+        # are we using 64-bit floats here?
+        gray_histogram = gray_histogram[0] / 255.0
 
         return gray_histogram
 
     # 2 channel Hue-Value histogram feature
     def get_hv_feature(self, image):
-        print("{} feature not implemented ".format(self.feature_type))
-        return 5
+        # Convert image to 8-bit Hue-Saturation-Value format
+        image = rgb2hsv(image)
+        image = np.uint8(image * 255)
+
+        # Create histograms for Hue and Value channels of size "bins"
+        hue_histogram = np.histogram(a=image[0], bins=range(0, self.bins))
+        value_histogram = np.histogram(a=image[2], bins=range(0, self.bins))
+
+        # Normalize histograms
+        # are we using 64-bit floats here?
+        hue_histogram = hue_histogram[0] / 255.0
+        value_histogram = value_histogram[0] / 255.0
+
+        # Concatenate Hue and Value histograms to get the HV feature vector
+        hv_feature = np.concatenate(hue_histogram, value_histogram, axis=None)
+
+        return hv_feature
 
     # 3 channel Red-Green-Blue histogram feature
     def get_rgb_feature(self, image):
