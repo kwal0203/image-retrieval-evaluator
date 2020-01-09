f59849687d922417f83de32eef45036f6638ebfb
Kane Walter <kwal0203@gmail.com>
2020-01-09 11:55:23 +1100

    Basic feature driver implementation complete

--- feature/feature_utils.py
+++ feature/feature_utils.py
@@ -1,8 +1,6 @@
-import os
-import json
-import glob
-
-from PIL import Image
+from json import load
+from skimage import io
+from os import listdir, path, getcwd
 from feature.feature_defintions.Histogram import Histogram
 
 # Constants
@@ -34,17 +32,34 @@ HISTOGRAM_BINS = 256
 # }
 
 
+def test_config_dict(config_dict):
+    print("----- TEST CONFIG DICT -----")
+    print("input:        ", config_dict['input'])
+    print("output:       ", config_dict['output'])
+    print("feature_name: ", config_dict['feature_name'])
+    print("feature_path: ", config_dict['feature_path'])
+    print("--- END TEST CONFIG DICT ---")
+
+
+def test_histogram_object(histogram_object):
+    print("----- TEST HISTOGRAM OBJECT -----")
+    print("Histogram name: ", histogram_object.histogram_type)
+    print("Histogram bins: ", histogram_object.bins)
+    print("--- END TEST HISTOGRAM OBJECT ---")
+
+
 # Parse config file
 def json_read():
-    config_path = os.path.join(os.getcwd(), 'params.json')
-    assert (os.path.isfile(config_path))
+    config_path = path.join(getcwd(), 'feature_params.json')
+    assert(path.isfile(config_path)), "{} does not exist".format(config_path)
+    print("Config file path: ", config_path)
 
     with open(config_path) as f:
-        config_file = json.load(f)
-        input_path = config_file['data_path_base']
-        input_name = config_file['data_name']
-        output_path = config_file['index_path_base']
-        output_name = config_file['index_name']
+        config_file = load(f)
+        input_path = config_file['input_path_base']
+        input_name = config_file['input_name']
+        output_path = config_file['output_path_base']
+        output_name = config_file['output_name']
         feature_path = config_file['feature_path_base']
         feature_name = config_file['feature_name']
 
@@ -59,13 +74,13 @@ def json_read():
 
 # Instantiate object for requested feature
 def feature_object(config):
-    feature_type = config['feature_name']
+    feature_name = config['feature_name']
     feature_path = config['feature_path']
 
-    if "histogram" in feature_type:
-        _feature = Histogram(HISTOGRAM_BINS, feature_type)
+    print(feature_name + " selected")
+    if "histogram" in feature_name:
+        _feature = Histogram(HISTOGRAM_BINS, feature_name)
     else:
-        print("not histogram")
         _feature = None
 
     return _feature
@@ -74,20 +89,26 @@ def feature_object(config):
 # Iterate through all images and write feature to CSV for each one
 def index_create(config, feature):
     output_path = config['output']
-    data_path = config['input']
-    with open(output_path, "w") as f:
+    input_path = config['input']
+    assert(not path.isfile(output_path)), "{} exists".format(output_path)
+
+    with open(output_path, "w+") as f:
         cnt = 0
-        for path in glob.glob(data_path + "*.jpg"):
-            img_id = path[path.rfind("/") + 1:]
-            image = Image.open(path)
-            feature_vector = feature.get_feature(image)
+
+        for filename in listdir(input_path):
+            # Filename without file extension used as name of feature in CSV
+            img_id = filename[:-4]
+
+            # Calculate feature vector
+            img = io.imread(input_path + filename)
+            feature_vector = feature.get_feature(img)
             feature_vector = [str(f) for f in feature_vector]
 
-            # Write each feature to CSV
-            _feature = "{},{}\n".format(img_id[:-4], ",".join(feature_vector))
+            # Turn feature into string and write it to CSV file
+            _feature = "{},{}\n".format(img_id, ",".join(feature_vector))
             f.write(_feature)
             cnt += 1
-            if cnt % 100 == 0:
+            if cnt % 1 == 0:
                 print("Feature file: {}, idx: {}".format(img_id, cnt))
 
 
@@ -95,13 +116,11 @@ def index_create(config, feature):
 def feature_driver_run():
     # Read input JSON
     config = json_read()
+    test_config_dict(config)
 
     # Instantiate feature object
     feature = feature_object(config)
-
-    # Determine image format in given directory
-
-    # Convert all images to JPG
+    test_histogram_object(feature)
 
     # Apply get_feature() to each image
     index_create(config, feature)
